cf90db2bdf46b781ff480423b4f3ef7f2446de98
Katerina <Kateryna.e.nikitina@gmail.com>
2023-09-07 20:04:10 -0600

    server settings added

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
+import mongoose from 'mongoose';
 import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+// const DB_HOST = 'mongodb+srv://Kateryna:H4ymUwsRaebL75m2@cluster0.8rqlkqp.mongodb.net/my-contacts?retryWrites=true&w=majority';
+
+mongoose.connect(process.env.DB_HOST)
+.then (() => {
+  app.listen(3000, () => {
+    console.log("Server running. Use our API on port: 3000")
+  })
 })
+.catch(error => {
+  console.log(error.message);
+  process.exit(1);
+}) ;
+
+
+
+// H4ymUwsRaebL75m2
